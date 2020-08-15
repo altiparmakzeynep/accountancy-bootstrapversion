00b201b3ad7f0a20b32db4cb6e3259493509d1f9
selinsnmz <selinsonmez111@gmail.com>
2020-08-15 16:03:39 +0300

    Added Popups

--- src/js/customers.js
+++ src/js/customers.js
@@ -1,130 +1,18 @@
 var id = localStorage.getItem("id", id);
 
 
-getData();
+document.querySelector('.customersDetailButton').addEventListener('click',Customers);
 
+document.querySelector('.suppliersDetailButton').addEventListener('click',Suppliers);
 
-
-function getData() {
-    var url = `http://192.168.1.108:3000/api/v1/customers/${id}`;
-    //var url = `http://e3b5dab837cc.ngrok.io/api/v1/customers/${id}`;
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", url, true);
-    xhr.onload = function() {
-
-
-        var post = JSON.parse(this.response);
-        console.log(post.data.customers);
-
-        var array = post.data.customers;
-        var totalAmount = post.data.userbalance.totalMoney;
-        console.log(post.data);
-        console.log(totalAmount);
-
-
-
-        ul = document.createElement('ul');
-
-
-        document.getElementById('rightBackground').appendChild(ul);
-
-        array.forEach(function(item) {
-            let div = document.createElement('div');
-            div.classList = "customers";
-
-            ul.appendChild(div);
-            div = "";
-
-
-            //FOR LOOP
-            for (var i = 0; i < array.length; i++) {
-
-
-                //Every buttons have own id from customers array
-                div += `
-                <button class="customers" id = "${array[i].id}" onclick="showData(this)">
-                    <p class="companyName">Şirket adı: ${array[i].customerName}</p>
-                    <p class="companyName">Şirket ünvanı: ${array[i].customerInfo}</p>
-                  
-                    <div class="deleteCustomer">
-                        <button class="deleteButton" ><img src=" ./assets/img/delete.png " width="9" height="9"></button>
-                    </div>
-                </button>`;
-
-                //div.innerHTML += array[i].customerName;
-                document.querySelector("#rightBackground").innerHTML = div;
-
-
-            }
-
-            userInfo = "";
-            userInfo += `
-            <p class="nameSurname">${post.data.fullName}</p>
-            <p class="homePageCompanyName">${post.data.companyName}</p>`;
-            document.querySelector("#userInfo").innerHTML = userInfo;
-
-            topInfo = "";
-            topInfo += `
-            <p class="inWithKDV">KDV'li Alınan:  ${post.data.userbalance.inMoney}</p>
-            <p class="amountofKDV">KDV Miktarı: ${post.data.userbalance.amountVAT}</p>
-            <p class="inWithoutKDV">KDV'siz Alınan: ${post.data.userbalance.inMoneyVAT}</p>
-            <p class="out">Ödenen: ${post.data.userbalance.outMoney}</p>`;
-            document.querySelector("#topInfo").innerHTML = topInfo;
-
-
-            totalAmount = "";
-            totalAmount += `
-            <p class="total">Toplam Bakiye: ${post.data.userbalance.totalMoney}</p>
-            `;
-            document.querySelector("#leftLittleWhite").innerHTML = totalAmount;
-
-
-            sessionStorage.setItem('totalAmount', post.data.userbalance.totalMoney);
-
-        });
-
-
-
-
-
-
-
-
-    }
-    xhr.send();
+function Customers(){
+    var category = "true";
+    localStorage.setItem("category",category);
+    // window.location ="suppliersAndCustomersDetail.html"
 }
 
-
-function showData(item) {
-    var cID = item.id;
-    sessionStorage.setItem("cID", cID);
-    console.log(cID);
-    window.location = "detail.html";
-
-
+function Suppliers(){
+    var category = "false";
+    localStorage.setItem("category",category);
+    // window.location = "suppliersAndCustomersDetail.html"
 }
-
-
-// getUser();
-
-// function getUser() {
-//     var cID = sessionStorage.getItem('cID');
-//     var url = `http://192.168.1.108:3000/api/v1/customers/${id}/details/${cID}`;
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", url, true);
-//     xhr.onload = function() {
-//         var post = JSON.parse(this.response);
-//         console.log(post.customer);
-
-//         div = "";
-//         div += `
-//         <p class="nameSurname"></p>
-//         <p class="homePageCompanyName"></p>`;
-//         document.querySelector("#leftInBackground").innerHTML = div;
-
-//     }
-//     xhr.send();
-
-
-// }
